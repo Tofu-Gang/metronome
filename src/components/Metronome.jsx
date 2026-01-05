995e463f196cfeb549ae854f1fed00d9ef049833
Tofu Gang <tofugangsw@gmail.com>
2026-01-05 23:04:45 +0100

    add metronome arm, position it correctly and set its color and size

--- src/components/Metronome.jsx
+++ src/components/Metronome.jsx
@@ -5,6 +5,7 @@ function Metronome() {
         <div className="fixed bottom-0 inset-x-0 mx-auto max-w-lg bg-gray-100 text-[#e4f876] font-[Rubik]">
             <h1 className="text-9xl text-center">120</h1>
             <img src={trapezoid} alt="An's trapezoid"/>
+            <div className="fixed bottom-0 inset-x-0 mx-auto inline-block w-3.5 h-112.5 bg-[#e4f876] origin-bottom-center rounded-xl mb-1"></div>
         </div>
     );
 }
