658bf624e37dd76579f4624ddf8adeb576b75f2a
Tofu Gang <tofugangsw@gmail.com>
2026-01-08 18:19:21 +0100

    refactoring

---
+++ src/icons/minus.jsx
@@ -0,0 +1,20 @@
+// https://www.svgviewer.dev/
+const minusIcon = <>
+    {/*<?xml version="1.0" encoding="utf-8"?>*/}
+    {/*License: PD. Made by stephenhutchings: https://github.com/stephenhutchings/typicons.font*/}
+    <svg
+        fill="#e4f876"
+        width="50px"
+        height="50px"
+        viewBox="0 0 24 24"
+        version="1.2"
+        baseProfile="tiny"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M18 11h-12c-1.104 0-2 .896-2 2s.896 2 2 2h12c1.104 0 2-.896 2-2s-.896-2-2-2z"
+        />
+    </svg>
+</>
+
+export default minusIcon;
